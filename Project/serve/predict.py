0a9c20c009dc9eb23f5d1bec2f0cde2eea16e0db
EC2 Default User <ec2-user@ip-172-16-46-101.us-east-2.compute.internal>
2020-10-23 21:25:14 +0000

    Deployment Project - final version - submitted to Udacity

--- Project/serve/predict.py
+++ Project/serve/predict.py
@@ -72,8 +72,8 @@ def predict_fn(input_data, model):
 
     data_X = None
     data_len = None
-    input_data_words = review_to_words(input_data)
-    data_X, data_len = convert_and_pad(model.word_dict, input_data_words)
+    input_data_to_words = review_to_words(input_data)
+    data_X, data_len = convert_and_pad(model.word_dict, input_data_to_words)
 
     # Using data_X and data_len we construct an appropriate input tensor. Remember
     # that our model expects input data of the form 'len, review[500]'.
@@ -91,8 +91,9 @@ def predict_fn(input_data, model):
     
     with torch.no_grad():
         output = model.forward(data)
+        output = output.to("cpu")
 
     result = np.round(output.numpy())
-    print(result)
+#     print(result)
 
     return result
